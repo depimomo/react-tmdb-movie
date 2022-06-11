8b4e6617529b5b8bf2009aea3936f94d9493f26e
Monica Kristiadi <monica.kristiadi@tech.jago.com>
2022-06-11 12:19:39 +0700

    create navbar

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+import { ThemeProvider } from '@mui/material';
+
+import Navbar from './components/Navbar';
+import theme from './themes/theme';
+
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        Movie app
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Navbar></Navbar>
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/themes/theme.js
@@ -0,0 +1,15 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        type: 'dark',
+        primary: {
+          main: '#E26D5A',
+        },
+        secondary: {
+          main: '#EFCA08',
+        },
+      },
+});
+
+export default theme;
