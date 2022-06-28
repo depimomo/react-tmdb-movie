d63ba023a208b7cc4c33a5cbb55f2cc2445b0788
Monica Kristiadi <monica.kristiadi@tech.jago.com>
2022-06-28 18:30:03 +0700

    handle sort when movies is not ready yet

--- src/containers/MovieList.js
+++ src/containers/MovieList.js
@@ -8,12 +8,14 @@ import MovieCard from '../components/MovieCard';
 const MovieList = () => {
     const [queryParams, setQueryParams] = useSearchParams();
     const [movies, setMovies] = useState([]);
+    const [moviesReady, setMoviesReady] = useState(false);
 
     useEffect(() => {
         const fetchMovies = async () => {
             try {
                 const fetchedMovies = await tmdb.get("trending/movie/week");
                 setMovies(fetchedMovies.data.results);
+                setMoviesReady(true);
             } catch (error) {
                 console.log(error);
             }
@@ -23,20 +25,21 @@ const MovieList = () => {
     }, []);
 
     useEffect(() => {
+        if (!moviesReady) return;
         const sortMovies = (type) => {
             if (type === 'asc') {
-                const sorted = movies.sort((a, b) => a.vote_average - b.vote_average);
+                const sorted = [...movies].sort((a, b) => a.vote_average - b.vote_average);
                 setMovies(sorted);
             }
             if (type === 'desc') {
-                const sorted = movies.sort((a, b) => b.vote_average - a.vote_average);
+                const sorted = [...movies].sort((a, b) => b.vote_average - a.vote_average);
                 setMovies(sorted);
             }
         }
 
         sortMovies(queryParams.get('sort'));
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [queryParams]);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [queryParams, moviesReady]);
 
     const setSortParam = (type) => {
         queryParams.set("sort", type);
