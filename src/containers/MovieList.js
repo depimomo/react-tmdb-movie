6ff46afd648a4de32003f18fc1407ebe4dea83e9
Monica Kristiadi <monica.kristiadi@tech.jago.com>
2022-06-28 18:04:15 +0700

    get query param and do sort

--- src/containers/MovieList.js
+++ src/containers/MovieList.js
@@ -22,6 +22,22 @@ const MovieList = () => {
         fetchMovies();
     }, []);
 
+    useEffect(() => {
+        const sortMovies = (type) => {
+            if (type === 'asc') {
+                const sorted = movies.sort((a, b) => a.vote_average - b.vote_average);
+                setMovies(sorted);
+            }
+            if (type === 'desc') {
+                const sorted = movies.sort((a, b) => b.vote_average - a.vote_average);
+                setMovies(sorted);
+            }
+        }
+
+        sortMovies(queryParams.get('sort'));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [queryParams]);
+
     const setSortParam = (type) => {
         queryParams.set("sort", type);
         setQueryParams(queryParams);
