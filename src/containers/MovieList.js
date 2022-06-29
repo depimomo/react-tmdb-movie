a364e18bfc5dcd440a0ecbffadb7d68efe0fbe47
Monica Kristiadi <monica.kristiadi@tech.jago.com>
2022-06-29 14:21:38 +0700

    using functional sort with useMemo

--- src/containers/MovieList.js
+++ src/containers/MovieList.js
@@ -1,50 +1,48 @@
 import { Box, Button } from '@mui/material';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
 import tmdb from '../apis/tmdb';
 import MovieCard from '../components/MovieCard';
 
+// Sort comparators
+const sortVoteAverageAsc = (a, b) => a.vote_average - b.vote_average;
+const sortVoteAverageDesc = (a, b) => sortVoteAverageAsc(b, a);
+
+// Sorting utility function
+const sortVoteAverage = (sort = "asc") =>
+    sort === 'asc' ? sortVoteAverageAsc : sortVoteAverageDesc;
+
 const MovieList = () => {
     const [queryParams, setQueryParams] = useSearchParams();
     const [movies, setMovies] = useState([]);
-    const [moviesReady, setMoviesReady] = useState(false);
 
     useEffect(() => {
         const fetchMovies = async () => {
             try {
                 const fetchedMovies = await tmdb.get("trending/movie/week");
                 setMovies(fetchedMovies.data.results);
-                setMoviesReady(true);
             } catch (error) {
                 console.log(error);
             }
-        }
+        };
 
         fetchMovies();
     }, []);
 
-    useEffect(() => {
-        if (!moviesReady) return;
-        const sortMovies = (type) => {
-            if (type === 'asc') {
-                const sorted = [...movies].sort((a, b) => a.vote_average - b.vote_average);
-                setMovies(sorted);
-            }
-            if (type === 'desc') {
-                const sorted = [...movies].sort((a, b) => b.vote_average - a.vote_average);
-                setMovies(sorted);
-            }
-        }
-
-        sortMovies(queryParams.get('sort'));
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [queryParams, moviesReady]);
-
     const setSortParam = (type) => {
         queryParams.set("sort", type);
         setQueryParams(queryParams);
-    }
+    };
+
+    const sort = queryParams.get("sort");
+
+    const sortedMovies = useMemo(() => {
+      return movies
+        .slice()
+        .sort(sortVoteAverage(sort))
+    }, [movies, sort]);
+
 
     return (
         <Box sx={{
@@ -75,17 +73,17 @@ const MovieList = () => {
                     Desc
                 </Button>
             </Box>
-            <Box sx={{
-                display: 'flex',
-                flexDirection: 'row',
-                flexWrap: 'wrap',
-                justifyContent: 'space-between',
-            }}>
-                {
-                    movies.map(movie => (
-                        <MovieCard key={movie.title} movie={movie}></MovieCard>
-                    ))
-                }
+            <Box
+                sx={{
+                    display: "flex",
+                    flexDirection: "row",
+                    flexWrap: "wrap",
+                    justifyContent: "space-between"
+                }}
+            >
+                {sortedMovies.map((movie) => (
+                    <MovieCard key={movie.title} movie={movie} />
+                ))}
             </Box>
         </Box>
     );
