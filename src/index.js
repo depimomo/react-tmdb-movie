1e13b1ada40a4c8357702a298bd1b436fa3ce1e4
Monica Kristiadi <monica.kristiadi@tech.jago.com>
2022-09-29 18:48:29 +0700

    change to 6.4 way

--- src/index.js
+++ src/index.js
@@ -1,18 +1,52 @@
 import './index.css';
 
+import { Box } from '@mui/material';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, createRoutesFromElements, Link, Route, RouterProvider } from 'react-router-dom';
 
 import App from './App';
+import About from './containers/About';
+import MovieList from './containers/MovieList';
+import Pricing from './containers/Pricing';
+import Subscribed from './containers/Subscribed';
 import reportWebVitals from './reportWebVitals';
+
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path="/" element={<App/>}>
+    <Route path="/" element={<MovieList />} />
+    <Route path="about" element={<About />}>
+      <Route path="description" element={<Box sx={{ mt: 10 }}>Provides movies in your hand</Box>} />
+      <Route path="services" element={<Box sx={{ mt: 10 }}>Streaming movies, Indonesian film, and film review.</Box>} />
+    </Route>
+    <Route path="indonesian" element={<Box sx={{ mt: 10 }}>Halaman indonesian</Box>} />
+    <Route path="pricing" element={<Pricing />} />
+    <Route path="subscribed/:plan" element={<Subscribed />} />
+    <Route
+      path="*"
+      element={
+        <Box sx={{
+          display: 'flex',
+          margin: 10,
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'column',
+        }}>
+          <img
+            src="https://cdn3d.iconscout.com/3d/premium/thumb/404-error-4461124-3696774.png"
+            alt="404"
+          />
+          <p>You have reach the edge of universe</p>
+          <Link to="/">Take me home!</Link>
+        </Box>
+      }
+    />
+  </Route>));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
