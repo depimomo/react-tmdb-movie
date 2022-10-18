06a5082e7d1719b5480f57fd0ca07f6800567e61
Monica Kristiadi <monica.kristiadi@tech.jago.com>
2022-10-18 20:00:36 +0700

    change api key to process env

--- src/apis/tmdb.js
+++ src/apis/tmdb.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = 'df7934f249ad19ef751a3c7ae05883e7';
+const API_KEY = process.env.REACT_APP_TMDB_KEY;
 const baseUrl = 'https://api.themoviedb.org/3/';
 
 const tmdb = axios.create({
